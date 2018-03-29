7a6a6209bc41e6df9c84b19940c7cc7b12134165
Antonio Pugliese <33133173+apugliese36@users.noreply.github.com>
2018-03-29 15:42:48 -0400

    Update token secret

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ class App extends Component {
     const BASE_URL = 'https://api.spotify.com/v1/search?';
     let FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
     const ALBUM_URL = 'https://api.spotify.com/v1/artists/';
-    var accessToken = 'BQCG3oBB2yAuh1-STMBtwpfZ1_bZJ_l6qiTGEId66D3WnRRvN53NdYU70ov7ZKqWOg0KMoIbk1n_uYaV5Ado-H890lnLAS3qlmnJ-HIyqmoBnsFu8tdiDeypVjMecw0ciHSF2XvYW17FA9LS9gOU8t5L7iOFiacUIQ&refresh_token=AQB_svNOAiYl6A9o1M13KjBU_zv-_wOJeZotWUy149q147zmxkmxHsWP12ST8RWFemAAux9ji15HsDM1zKXJaVw8DZMrT2gY7E-_4NDySahSguZe21NwdtlKQBOt56tu95g'
+    var accessToken = 'secret'
 
     var myOptions = {
       method: 'GET',
